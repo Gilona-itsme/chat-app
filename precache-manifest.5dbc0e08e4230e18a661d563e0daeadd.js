2dd650c07b736e2066755295db5654f84987fdaa
Ilona <ilona.gil@outlook.com>
2021-05-24 16:43:28 +0300

    Updates

---
+++ precache-manifest.5dbc0e08e4230e18a661d563e0daeadd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b17a5b0650b12afd5fc",
+    "url": "/Gilona-itsme/chat-app.git/static/css/main.c8d039ad.chunk.css"
+  },
+  {
+    "revision": "5b17a5b0650b12afd5fc",
+    "url": "/Gilona-itsme/chat-app.git/static/js/main.bb86f6c0.chunk.js"
+  },
+  {
+    "revision": "b0355a7c555179baa4a8",
+    "url": "/Gilona-itsme/chat-app.git/static/js/runtime~main.6c7b95d3.js"
+  },
+  {
+    "revision": "cf8898ad3912f741dc0b",
+    "url": "/Gilona-itsme/chat-app.git/static/js/2.2d0f53ce.chunk.js"
+  },
+  {
+    "revision": "717d8f583d1cb8cc29b0ceca63dbe1ad",
+    "url": "/Gilona-itsme/chat-app.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Gilona-itsme/chat-app.git/precache-manifest.5dbc0e08e4230e18a661d563e0daeadd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Gilona-itsme/chat-app.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
